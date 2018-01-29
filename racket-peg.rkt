0f198d288422df3fe0e1dc0479af82c28ef3c659
rain1 <rain1@airmail.cc>
2018-01-29 16:50:20 +0000

    extend it to peg by adding callable named rules

--- racket-peg.rkt
+++ racket-peg.rkt
@@ -1,65 +1,177 @@
 #lang racket
 
-;; based on https://swtch.com/~rsc/regexp/regexp2.html
+(require (for-syntax racket/syntax))
+(require (for-syntax racket/match))
 
 (define-syntax-rule (pop! place)
-  (let ((top (car place)))
-    (set! place (cdr place))
+  (let ((top (car (unbox place))))
+    (set-box! place (cdr (unbox place)))
     top))
 
-(define-syntax-rule (push! thing place)
-  (set! place (cons thing place)))
+(define-syntax-rule (push! place thing)
+  (set-box! place (cons thing (unbox place))))
 
-(define-for-syntax (re:compile str fail alt exp lbl end)
-  (with-syntax ([str str] [fail fail] [alt alt] [lbl lbl] [end end])
-    (syntax-case exp (char seq choice star)
-        
+(define peg:string (make-parameter #f))
+(define peg:sp (make-parameter #f))
+(define peg:fail-stack (make-parameter #f))
+(define (peg:eof?) (= (unbox (peg:sp)) (- (string-length (peg:string)) 0)))
+
+(define (peg:string@)
+  (string-ref (peg:string) (unbox (peg:sp))))
+
+(define (inc! b n)
+  (set-box! b (+ (unbox b) n)))
+
+(define (string-contains-substring? str place contained)
+  (and (>= (string-length str) (+ place (string-length contained)))
+       (string=? (substring str place (+ place (string-length contained)))
+                 contained)))
+
+(define (char->string ch) (list->string (list ch)))
+
+(define (string-contains-char? str ch)
+  (string-contains? str (char->string ch)))
+
+(define (peg:join x y)
+  (cond ((and (string? x) (string? y)) (string-append x y))
+        ((and (list? x) (list? y)) (append x y))
+        (else (list x y))))
+
+(define-for-syntax (peg:names exp)
+  (syntax-case exp (epsilon char string range seq choice star optional plus call name)
+    [(seq e1 e2) (append (peg:names #'e1) (peg:names #'e2))]
+    [(choice e1 e2) (append (peg:names #'e1) (peg:names #'e2))]
+    [(star e1) (peg:names #'e1)]
+    [(optional e1) (peg:names #'e1)]
+    [(plus e1) (peg:names #'e1)]
+    
+    [(name nm subexp) (cons #'nm (peg:names #'subexp))]
+
+    [else '()]))
+
+(define-for-syntax (peg:compile exp sk)
+  (with-syntax ([sk sk])
+    (syntax-case exp (epsilon char string range seq choice star optional plus call name)
+
+      [(epsilon)
+       #'(sk "")]
+      
       [(char c)
-       (list #'(lbl (lambda (sp)
-                      (if (char=? c (string-ref str sp))
-                          (end (+ sp 1))
-                          (fail)))))]
-        
+       #'(if (and (not (peg:eof?)) (char=? c (peg:string@)))
+             (begin
+               (inc! (peg:sp) 1)
+               (sk (char->string c)))
+             ((pop! (peg:fail-stack))))]
+      
+      [(range str)
+       #'(if (peg:eof?)
+             ((pop! (peg:fail-stack)))
+             (let ((c (peg:string@)))
+               (if (string-contains-char? str c)
+                   (begin
+                     (inc! (peg:sp) 1)
+                     (sk (char->string c)))
+                   ((pop! (peg:fail-stack))))))]
+      
+      [(string str)
+       (let ((str-len (string-length (syntax->datum #'str))))
+         #`(if (string-contains-substring? (peg:string) (unbox (peg:sp)) str)
+               (begin
+                 (inc! (peg:sp) #,str-len)
+                 (sk str))
+               ((pop! (peg:fail-stack)))))]
+      
       [(seq e1 e2)
-       (with-syntax ([(mid) (generate-temporaries #'(mid))])
-         (append (re:compile #'str #'fail #'alt #'e1 #'lbl #'mid)
-                 (re:compile #'str #'fail #'alt #'e2 #'mid #'end)))]
-        
+       #`(let ((mk (lambda (r1)
+                    (let ((sk^ (lambda (r2) (sk (peg:join r1 r2)))))
+                      #,(peg:compile #'e2 #'sk^)))))
+           #,(peg:compile #'e1 #'mk))]
+      
       [(choice e1 e2)
-       (with-syntax ([(lbl-1 lbl-2) (generate-temporaries #'(e1 e2))])
-         (append (list #'(lbl (lambda (sp)
-                                (alt (lambda () (lbl-2 sp)))
-                                (lbl-1 sp))))
-                 (re:compile #'str #'fail #'alt #'e1 #'lbl-1 #'end)
-                 (re:compile #'str #'fail #'alt #'e2 #'lbl-2 #'end)))]
-        
-      [(star e1)
-       (with-syntax ([(mid) (generate-temporaries #'(mid))])
-         (append (list #'(lbl (lambda (sp)
-hah                                (alt (lambda () (end sp)))
-                                (mid sp))))
-                 (re:compile #'str #'fail #'alt #'e1 #'mid #'lbl)))]
-        
-      [else
-       (raise-syntax-error "invalid re" (syntax->datum exp))])))
-  
-(define-syntax (re stx)
-  (syntax-case stx ()
-    [(_ exp)
-     (with-syntax ([(binding ...) (re:compile #'str #'fail #'alt #'exp #'start #'end)])
-       #'(lambda (str)
-           (let ((alternatives (list (lambda () #f))))
-             (letrec ((fail (lambda () ((pop! alternatives))))
-                      (alt (lambda (thunk) (push! thunk alternatives)))
-                      (end (lambda (sp)
-                             (display 'done)
-                             (display sp)
-                             (newline)))
-                      binding ...)
-               (start 0)))))]))
+       #`(let ((k1 (lambda () #,(peg:compile #'e1 #'sk)))
+               (k2 (lambda () #,(peg:compile #'e2 #'sk))))
+           (push! (peg:fail-stack) k2)
+           (k1))]
 
-(define t1 (re (char #\a)))
-(define t2 (re (seq (char #\a) (char #\b))))
-(define t3 (re (seq (char #\a) (seq (char #\b) (char #\c)))))
-(define t4 (re (seq (char #\a) (seq (choice (char #\b) (seq (char #\x) (char #\y))) (char #\c)))))
-(define t5 (re (seq (char #\s) (seq (char #\h) (seq (star (seq (char #\e) (char #\E))) (char #\p))))))
+      [(star e)
+       #`(letrec ((k* (lambda (res)
+                        (push! (peg:fail-stack) (lambda () (sk res)))
+                        (let ((k-next (lambda (r1)
+                                        (k* (peg:join res r1)))))
+                          #,(peg:compile #'e #'k-next)))))
+           (k* ""))]
+
+      [(optional e)
+       (peg:compile #'(choice e (epsilon)) #'sk)]
+      
+      [(plus e)
+       (peg:compile #'(seq e (star e)) #'sk)]
+      
+      [(call rule-name)
+       (with-syntax ([rule (format-id #'rule-name "peg-rule:~a" #'rule-name)])
+         #'(rule sk))]
+
+      [(name nm subexp)
+       #`(let ((sk! (lambda (res)
+                      (set! nm res)
+                      (sk res))))
+           #,(peg:compile #'subexp #'sk!))]
+      
+      [else (raise-syntax-error "invalid peg" (syntax->datum exp))])))
+
+(define-syntax (define-peg stx)
+  (syntax-case stx (name)
+    [(_ rule-name exp)
+     #'(define-peg rule-name (name res exp) res)]
+    [(_ rule-name exp action)
+     (with-syntax ([name (format-id #'rule-name "peg-rule:~a" #'rule-name)]
+                   [body (peg:compile #'exp #'sk^)]
+                   [nms (map (lambda (nm) #`(#,nm #f)) (peg:names #'exp))])
+       #'(define (name sk)
+           (let nms
+             (let ((sk^ (lambda (_) (sk action))))
+               body))))]))
+
+(define-syntax (peg stx)
+  (syntax-case stx ()
+    [(_ rule-name str)
+     (with-syntax ([name (format-id #'rule-name "peg-rule:~a" #'rule-name)]
+                   [err #'(error "parse failed at location" (unbox (peg:sp)))])
+       #'(parameterize ([peg:string str]
+                        [peg:sp (box 0)]
+                        [peg:fail-stack (box (list (lambda () err)))])
+           (name (lambda (res)
+                   (display "success! ")
+                   (display res)
+                   (newline)))))]))
+
+(define-peg digit
+  (range "0123456789"))
+(define-peg nonzero-digit
+  (range "123456789"))
+(define-peg number
+  (name res (seq (call nonzero-digit) (star (call digit))))
+  (string->number res))
+
+(define-peg sum
+  (seq (name n1 (call number))
+       (optional (seq (name op (choice (char #\+) (char #\-)))
+                      (name n2 (call sum)))))
+  (if op
+      `(,op ,n1 ,n2)
+      n1))
+
+(define-peg sheep
+  (seq (call sh) (seq (call eE2) (char #\p))))
+(define-peg sh (string "sh"))
+(define-peg eE
+  (choice (seq (string "eE")
+               (call eE))
+          (epsilon)))
+(define-peg eE2
+  (plus (choice (string "eE")
+                (string "Ee"))))
+
+(peg sheep "sheEeEeEp")
+(peg sum "3000974+53+4-23424")
+
