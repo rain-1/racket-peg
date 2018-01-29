f55b458dc7324c9b70c40ddda2f211ab6ee0cef9
rain1 <rain1@airmail.cc>
2018-01-29 16:49:34 +0000

    reimplement it with letrec, add kleene star

--- racket-peg.rkt
+++ racket-peg.rkt
@@ -1,103 +1,65 @@
 #lang racket
 
-;; char c 	If the character SP points at is not c, stop this
-;;              thread: it failed. Otherwise, advance SP to the next
-;;              character and advance PC to the next instruction.
-;;
-;; match 	Stop this thread: it found a match.
-;;
-;; jmp x 	Jump to (set the PC to point at) the instruction at x.
-;;
-;; split x, y 	Split execution: continue at both x and y. Create a
-;;              new thread with SP copied from the current thread. One
-;;              thread continues with PC x. The other continues with PC
-;;              y. (Like a simultaneous jump to both locations.)
+;; based on https://swtch.com/~rsc/regexp/regexp2.html
 
-(struct ctx (str [sp #:mutable]))
-(define vm-ctx (make-parameter #f))
+(define-syntax-rule (pop! place)
+  (let ((top (car place)))
+    (set! place (cdr place))
+    top))
 
-(define (vm-ctx:@sp)
-  (let ((cx (vm-ctx)))
-    (string-ref (ctx-str cx) (ctx-sp cx))))
-(define (vm-ctx:sp++)
-  (let ((cx (vm-ctx)))
-    (set-ctx-sp! cx (+ (ctx-sp cx) 1))))
-(define (vm-ctx:push! )
-  (let ((cx (vm-ctx)))
-    )))
+(define-syntax-rule (push! thing place)
+  (set! place (cons thing place)))
 
-
-(define-struct (match-fail exn:fail:user) ())
-(define-syntax (vm:fail stx)
+(define-for-syntax (re:compile str fail alt exp lbl end)
+  (with-syntax ([str str] [fail fail] [alt alt] [lbl lbl] [end end])
+    (syntax-case exp (char seq choice star)
+        
+      [(char c)
+       (list #'(lbl (lambda (sp)
+                      (if (char=? c (string-ref str sp))
+                          (end (+ sp 1))
+                          (fail)))))]
+        
+      [(seq e1 e2)
+       (with-syntax ([(mid) (generate-temporaries #'(mid))])
+         (append (re:compile #'str #'fail #'alt #'e1 #'lbl #'mid)
+                 (re:compile #'str #'fail #'alt #'e2 #'mid #'end)))]
+        
+      [(choice e1 e2)
+       (with-syntax ([(lbl-1 lbl-2) (generate-temporaries #'(e1 e2))])
+         (append (list #'(lbl (lambda (sp)
+                                (alt (lambda () (lbl-2 sp)))
+                                (lbl-1 sp))))
+                 (re:compile #'str #'fail #'alt #'e1 #'lbl-1 #'end)
+                 (re:compile #'str #'fail #'alt #'e2 #'lbl-2 #'end)))]
+        
+      [(star e1)
+       (with-syntax ([(mid) (generate-temporaries #'(mid))])
+         (append (list #'(lbl (lambda (sp)
+hah                                (alt (lambda () (end sp)))
+                                (mid sp))))
+                 (re:compile #'str #'fail #'alt #'e1 #'mid #'lbl)))]
+        
+      [else
+       (raise-syntax-error "invalid re" (syntax->datum exp))])))
+  
+(define-syntax (re stx)
   (syntax-case stx ()
-    [(_)
-     #`(raise (make-match-fail "unknown reason" (current-continuation-marks)))]))
+    [(_ exp)
+     (with-syntax ([(binding ...) (re:compile #'str #'fail #'alt #'exp #'start #'end)])
+       #'(lambda (str)
+           (let ((alternatives (list (lambda () #f))))
+             (letrec ((fail (lambda () ((pop! alternatives))))
+                      (alt (lambda (thunk) (push! thunk alternatives)))
+                      (end (lambda (sp)
+                             (display 'done)
+                             (display sp)
+                             (newline)))
+                      binding ...)
+               (start 0)))))]))
 
-(define-syntax (vm:char stx)
-  (syntax-case stx ()
-    [(_ chr)
-     #`(if (char=? chr (vm-ctx:@sp))
-           (vm-ctx:sp++)
-           (vm:fail))]))
-(define-syntax (vm:split stx)
-  (syntax-case stx ()
-    [(_ lbl-1 lbl-2)
-     #`()]))
-
-(define-syntax (re:compile stx)
-  (syntax-case stx (epsilon seq choice
-                            lbl-1 lbl-2 lbl-3)
-    [(_ (epsilon))
-     #'(begin/goto
-         #t)]
-    [(_ (seq e1 e2))
-     #'(begin/goto
-         (re:compile e1)
-         (re:compile e2))]
-    [(_ (seq e1 e2 e3 ...))
-     #'(begin/goto
-         (re:compile e1)
-         (re:compile e2) 
-         (re:compile e3) ...)]
-    [(_ (choice e1 e2))
-     #'(begin/goto
-         (vm:split lbl-1 lbl-2)
-         (label lbl-1)
-         (re:compile e1)
-         (goto lbl-3)
-         (label lbl-2)
-         (re:compile e2)
-         (label lbl-3))]
-    [(_ ch)
-     (let ((ch^ (syntax->datum #'ch)))
-       (cond [(char? ch^)
-              #'(vm:char ch)]
-             [(string? ch^)
-              (let ((string^ (foldr (lambda (x ys) #`(seq #,x #,ys))
-                                    #'(epsilon)
-                                    (string->list ch^))))
-                #`(re:compile #,string^))]
-             [else
-              (raise-syntax-error 're "expected a character" ch^)]))]))
-
-(define (test-re str body)
-  (with-handlers ([match-fail? (lambda (e)
-                                 #f)])
-    (parameterize ([vm-ctx (ctx str 0)])
-      (body))))
-
-(define t1 (test-re "foobar"
-                    (lambda ()
-                      (re:compile (seq #\a #\b)))))
-(define t1b (test-re "abba"
-                    (lambda ()
-                      (re:compile (seq #\a #\b)))))
-(define t2 (test-re "foobar"
-                    (lambda ()
-                      (re:compile (seq #\f "oobar")))))
-(define t2b (test-re "foobar"
-                     (lambda ()
-                      (re:compile (seq #\f "oobaz")))))
-(define t2c (test-re "foobar"
-                     (lambda ()
-                       (re:compile (seq #\f "ooba" (choice #\z #\r))))))
+(define t1 (re (char #\a)))
+(define t2 (re (seq (char #\a) (char #\b))))
+(define t3 (re (seq (char #\a) (seq (char #\b) (char #\c)))))
+(define t4 (re (seq (char #\a) (seq (choice (char #\b) (seq (char #\x) (char #\y))) (char #\c)))))
+(define t5 (re (seq (char #\s) (seq (char #\h) (seq (star (seq (char #\e) (char #\E))) (char #\p))))))
