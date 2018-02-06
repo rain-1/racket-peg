99f148a69f20da02c66657ca56467ec879d8cab8
rain1 <rain1@airmail.cc>
2018-02-06 19:32:01 +0000

    move docs to scribblings

--- info.rkt
+++ info.rkt
@@ -1,4 +1,8 @@
 #lang info
 (define version "0.1")
 (define collection "peg")
-(define scribblings '(("scribblings/peg.scrbl")))
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/peg.scrbl" ())))
+(define pkg-desc "A PEG parser generator")
