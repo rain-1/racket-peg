22416308170b7909a90157c009e770cf8905b2e5
rain1 <rain1@airmail.cc>
2018-08-01 02:42:40 +0100

    attempt to make docs work by moving info file

--- peg/info.rkt
+++ peg/info.rkt
@@ -3,5 +3,5 @@
 (define collection "peg")
 (define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/peg.scrbl" ())))
+(define scribblings '(("../scribblings/peg.scrbl" ())))
 (define pkg-desc "A PEG parser generator")
