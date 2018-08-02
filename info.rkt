4ccecf4476d4d2c1b1569e71bb93356c11440876
rain1 <rain1@airmail.cc>
2018-08-02 17:33:45 +0100

    ignore path

--- info.rkt
+++ info.rkt
@@ -4,4 +4,5 @@
 (define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/peg.scrbl" ())))
+(define compile-omit-paths '("peg-src"))
 (define pkg-desc "A PEG parser generator")
