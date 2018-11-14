6d06d7db5d87ba367954edf7bf4e34f00ebd9d01
rain1 <rain1@airmail.cc>
2018-11-14 23:25:43 +0000

    port push-pop-boxes.rkt to guile

--- push-pop-boxes.rkt
+++ push-pop-boxes.rkt
@@ -1,13 +1,23 @@
 (define-module (racket-peg push-pop-boxes)
+  #:use-module (srfi srfi-111)
   #:export (push! pop!))
 
 ;;;;
 ;; push and pop for boxes
 
-(define-syntax-rule (push! place thing)
-  (set-box! place (cons thing (unbox place))))
+(define-syntax push!
+  (syntax-rules ()
+    ((push! place thing)
+     (set-box! place (cons thing (unbox place))))))
 
-(define-syntax-rule (pop! place)
-  (let ((top (car (unbox place))))
-    (set-box! place (cdr (unbox place)))
-    top))
+(define-syntax pop!
+  (syntax-rules ()
+    ((pop! place)
+     (let ((top (car (unbox place))))
+       (set-box! place (cdr (unbox place)))
+       top))))
+
+;scheme@(guile-user)> (load "push-pop-boxes.rkt")
+;scheme@(guile-user)> (use-modules (racket-peg push-pop-boxes) (srfi srfi-111))
+;scheme@(guile-user)> (let ((x (box '()))) (push! x 3) x)
+;$1 = #<box 55c5617c2c80 value: (3)>
