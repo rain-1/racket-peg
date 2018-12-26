185f47affa4d7dcd722a1c914ff6e2114c206d1b
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-12-26 02:07:57 -0200

    case of sucess in this version of debug

--- tests/test-debug.rkt
+++ tests/test-debug.rkt
@@ -5,8 +5,12 @@
 
 (define-peg number (name v (+ (range #\0 #\9))) (string->number v))
 
+(define-peg sum (and (name v1 number) "+" (name v2 number)) (+ v1 v2))
+
 (define-peg exp (or (and number (drop "+") exp) number))
 
 (peg exp "12" #t)
 
 (peg exp "12+13" #t)
+
+(peg sum "12+13" #t)
